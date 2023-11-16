9647005da76ba591f4ea076d7f5c9f02564ae330
Fatima Naveed <fatimapauly91@gmail.com>
2023-11-16 16:32:27 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 5 };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Bind the function by this(Counter)
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  // Function like class methods
+  handleDecrement() {
+    this.setState((obj) => {
+      return { count: obj.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((obj) => {
+      return { count: obj.count + 1 };
+    });
+  }
+
+  render() {
+    const dated = new Date("17 june 2025");
+    dated.setDate(dated.getDate() + this.state.count);
+    return (
+      <>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{dated.toDateString()}</span>
+        <button onClick={this.handleIncrement}>+</button>
+      </>
+    );
+  }
 }
 
-export default App;
+export default Counter;
